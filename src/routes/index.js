eaa46dac5993ed2f236010f4aab6d64dd37c4c5e
Susetya Mulia Pambudi <susetyamulia@gmail.com>
2020-05-31 01:56:43 +0700

    Initialize Project

---
+++ src/routes/index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import { testEnvironmentVariable } from '../settings';
+const indexRouter = express.Router();
+
+/* GET home page. */
+indexRouter.get('/', (req, res) => {
+  return res.status(200).json({ message: testEnvironmentVariable });
+});
+
+export default indexRouter;
